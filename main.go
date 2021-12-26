f38aea01d3a252ed10b148d4fe60574f764e47f2
fuRan <NgeKaworu@gmail.com>
2021-12-26 13:06:41 +0800

    redis addr

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	}
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
